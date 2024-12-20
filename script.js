8ddc81e08b529291cbd1514922577c188d265d8c
rishabhy043 <rishabh.knipss043@gmail.com>
2024-12-20 21:18:33 +0530

    completed

---
+++ script.js
@@ -0,0 +1,20 @@
+const basket1 = document.querySelector(".basket1");
+const basket2 = document.querySelector(".basket2");
+const img1 = document.querySelector(".img1");
+const img2 = document.querySelector(".img2");
+value1 = parseInt(basket1.innerHTML);
+value2 = parseInt(basket2.innerHTML);
+
+img1.addEventListener("click" , () =>{
+value1--;
+basket1.innerHTML = value1;
+value2 ++;
+basket2.innerHTML = value2;
+});
+
+img2.addEventListener("click" , () =>{
+    value2 --;
+    basket2.innerHTML = value2;
+    value1 ++;
+    basket1.innerHTML = value1;
+    })
