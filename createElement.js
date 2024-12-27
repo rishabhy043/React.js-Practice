ba0c359a84694e3f5c6c56b47a6f83846f03199c
rishabhy043 <rishabh.knipss043@gmail.com>
2024-12-27 23:37:24 +0530

    complete

--- createElement.js
+++ createElement.js
@@ -1,1 +1,8 @@
-const h1 = React.createElement("div" , {class:"class1" } )
+const h1 = React.createElement("div" , {className: "class1" , id : "id1" } ,"i am rishabh" )
+const h2 = React.createElement('h2' , {className:"abc"} , "i am b.tech student")
+const root = ReactDOM.createRoot(document.querySelector(".root"));
+
+root.render(h1)
+root.render(h2)
+// console.dir(h2);
+
