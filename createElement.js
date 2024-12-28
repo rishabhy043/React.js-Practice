df8930a52fe24e3c18ecff4e0e10e62ea307b34a
rishabhy043 <rishabh.knipss043@gmail.com>
2024-12-28 14:29:57 +0530

    learning

--- createElement.js
+++ createElement.js
@@ -10,7 +10,12 @@ root.render(h2)
 //    Creating element 
 
 const Element = React.createElement("div" , { class : "Elem2"} ,
-     [React.createElement("section" , {key:1} , [React.createElement("p" , {key:2} , "ReactDOM Code Breakdown:")])]
-    )
+[React.createElement("section" , {key:1} , [React.createElement("p" , {key:2} , "ReactDOM Code Breakdown:") ,
+    React.createElement("img" , {class : "img1" , src : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXTPuiJmJPHb-4ES2ESNMTU61N2Y1-vANe2w&s"} )
+]) , React.createElement("h1" , {} , "I AM HEADING ONE")]
+)
 
-    root.render(Element)
+ root.render(Element)
+
+const createimg= React.createElement("img" , {class : "img1" , src : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXTPuiJmJPHb-4ES2ESNMTU61N2Y1-vANe2w&s"} )
+// root.render(createimg)
